099d4c7f3224553e7915e88eaff8677c44fc2b2a
Kenyada Henderson-Ward <hendersonkenyada.kh@gmail.com>
2023-12-07 20:33:49 -0600

    added gregSays

--- script.js
+++ script.js
@@ -5,7 +5,8 @@
 // Declaring with 'var' will make them global
 // variables.
 
-var aliceSays, bobSays, carolSays, danSays, ellieSays, frankSays, harrySays, ivySays, jackSays;
+var aliceSays, bobSays, carolSays, danSays, gregSays,  ellieSays,
+ frankSays, harrySays, ivySays, jackSays;
 
 
 // this variable is set to a string.
@@ -14,8 +15,18 @@ let theMessage = "Kathy loves Larry";
 aliceSays = theMessage;
 bobSays = aliceSays;
 carolSays = bobSays;
+danSays = carolSays;
+ellieSays = danSays;
+frankSays = danSays;
+harrySays = ivySays;
+jackSays = ivySays;
+gregSays = carolSays;
+
+
+
+//i'm stuck on calling funtions
 
 
 // call setNames from 'messages.js'
 // to set the messages in the DOM.
-setMessages();
+setMessages()
